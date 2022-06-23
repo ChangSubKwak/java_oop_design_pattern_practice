c87ad9e4f22ed0a64ad5a7195cc79c66911d2dc1
changsubkwak <paradise1732@gmail.com>
2022-06-23 09:04:59 +0900

    feat: add original code in page 99

---
+++ src/main/java/com/original/CartForSongs.java
@@ -0,0 +1,35 @@
+package com.original;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CartForSongs {
+    List<Song> cart = new ArrayList<>();
+    
+    public double calculateTotalPrice() {
+        double total = 0.0;
+        Iterator<Song> itr = cart.iterator();
+        
+        while (itr.hasNext()) {
+            Song s = itr.next();
+            
+            if (s.getDiscountMode().equals("OnSale")) {
+                total += 0.9 * s.getPrice();
+            }
+            else if (s.getDiscountMode().equals("TodayEvent")) {
+                total += 0.7 * s.getPrice();
+            }
+            else {
+                total += s.getPrice();
+            }
+            System.out.println("total = " + total);
+        }
+        
+        return total;
+    }
+    
+    public void add(Song song) {
+        cart.add(song);
+    }
+}
